4e17369586cdf39f0d9118db4d24667e87df7c6a
Hari Purnomo Susanto <haripurnomosusanto@gmail.com>
2023-03-26 18:40:02 +0000

    version 0.3.4

---
+++ R/irtawsi.R
@@ -0,0 +1,12 @@
+#'
+#' @title Items Response Theory Analysis with Steps and Interpretation
+#' @return Nothing
+#' @description Analysis of Dichotomous and polytomous data using unidimensional Item Response Theory model (Chalmers (2012) <doi:10.18637/jss.v048.i06>) with user friendly Graphical User Interface. Suitable for beginners who are learning Item Response Theory.
+#' @details launches 'shiny' interface
+#' @keywords IRT
+#'
+#' @export
+
+irtawsi <- function() {
+  shiny::runApp(appDir = system.file("irtawsi", package="irtawsi"))
+}
